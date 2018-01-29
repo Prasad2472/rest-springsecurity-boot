2524cbbccb3b6958e9fff1e63d960515f8740793
pboini <pboini@DIN86000363.corp.capgemini.com>
2018-01-29 14:36:05 +0530

    REST API + Spring Boot + Spring Security

---
+++ src/test/java/in/rest/security/restbootsecurity/RestBootSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package in.rest.security.restbootsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestBootSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
